1a8d0b57f912e4f57345ef2bdc152c06ce5cd761
priyamrajput <priyamrajput99@gmail.com>
2026-08-04 15:19:56 +0530

    Initialize project dependencies and server configuration
    - Add Shadcn UI and supporting libraries to the client
    - Configure theme provider and global styles for the client
    - Set up Drizzle, Express, and database utilities for the server
    - Remove default Next.js boilerplate files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,19 @@
-console.log("hello there")
+import { createServer } from "http";
+import { createApplication } from "./app.js";
+import { env } from "./common/config/env.js";
+import { connectDB } from "./db/index.js";
+
+async function startServer() {
+  try {
+    const server = createServer(createApplication());
+
+    server.listen(env.PORT, () => {
+      connectDB();
+      console.log(`http server is listing at PORT: ${env.PORT}`);
+    });
+  } catch (error: unknown) {
+    console.error(error);
+  }
+}
+
+startServer();
