d60886d4cad3ae4b92298cae2c7a19d4c0f91738
priyamrajput <priyamrajput99@gmail.com>
2026-09-09 17:11:15 +0530

    Configure React Query provider and wrap root layout

---
+++ client/components/provider/query-provider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import React, { useState } from "react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
